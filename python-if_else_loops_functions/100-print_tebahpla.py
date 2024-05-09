03d7a71ad9922f1afdc66022662ccd8263fecffa
luissoto7710 <luissoto7710@gmail.com>
2024-05-08 23:36:58 -0400

    My Commit

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(122 - i) if i % 2 == 0 else chr(90 - (i - 1)) for i in range(26)), end='')
+print("{}".format(''.join(chr(122 - i) if i % 2 == 0 else chr(90 - (i - 1)) for i in range(26))), end='')
