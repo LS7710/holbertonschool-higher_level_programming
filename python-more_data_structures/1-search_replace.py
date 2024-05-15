3df7014834b34af3be26925003143c33242e9e5c
luissoto7710 <luissoto7710@gmail.com>
2024-05-14 22:45:33 -0400

    My Commit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """
-    Compute the square of each element in a 2D matrix.
 
-    Args:
-    matrix (list of list of int): The input 2D matrix.
-
-    Returns:
-    list of list of int: A new matrix where each element is the square of the element from the input matrix.
-    """
     return [[x**2 for x in row] for row in matrix]
