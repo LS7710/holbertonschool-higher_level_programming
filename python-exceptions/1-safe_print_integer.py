1091343320e0a7e4da3993149e57357b8e70b7e1
luissoto7710 <luissoto7710@gmail.com>
2024-05-16 09:44:22 -0400

    My Commit

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+    except IndexError:
+        print()
+    finally:
+        return count
     
