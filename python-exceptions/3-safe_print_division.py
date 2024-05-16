cb6773f4a664bfa6cb6bd59f733a358fc8a05dfb
luissoto7710 <luissoto7710@gmail.com>
2024-05-16 11:51:17 -0400

    My Commit

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = None
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-        return result
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError, IndexError):
+
+            if isinstance(my_list[i], int):
+
+                raise
+            continue
+    print()
+    return count
