bfa8eeb8aa18b689be180e26e595329a3c6c8edd
luissoto7710 <luissoto7710@gmail.com>
2024-05-17 22:18:33 -0400

    My Commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 This module defines a Square class.
-The purpose of this class is to represent a square.
+The purpose of this class is to represent a square geometric figure.
 """
 
 class Square:
     """
     Represents an empty square.
-    This class doesn't contain any attributes or methods yet.
+    
+    Currently, this class does not contain any attributes or methods. serves
+    as a placeholder for future functionality.
     """
     pass
