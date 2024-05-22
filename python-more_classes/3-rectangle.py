62af8915d3e52656a38bb3f51f75fe32761007df
luissoto7710 <luissoto7710@gmail.com>
2024-05-21 23:31:47 -0400

    My Commit

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -84,6 +84,6 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return ""
-        rectangle_str = "\n".join(["#" * self.width for _ in 
+        rectangle_str = "\n".join(["#" * self.width for _ in
                                    range(self.height)])
         return rectangle_str
