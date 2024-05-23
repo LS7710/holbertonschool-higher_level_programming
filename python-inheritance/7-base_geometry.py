cfaf863212d623f277b97160bd7e0bfff230fea0
luissoto7710 <luissoto7710@gmail.com>
2024-05-23 12:57:24 -0400

    My Commit

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""
-This module defines the BaseGeometry.
-"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+bg = BaseGeometry()
 
-class BaseGeometry:
-    '''
-    BaseGeometry class.
-    '''
-    def area(self):
-        '''
-        calculates area
-        '''
-        raise Exception('area() is not implemented')
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
 
-    def integer_validator(self, name, value):
-        '''
-        integer validator.
-        '''
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
