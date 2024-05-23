29ee8658f35c984424fe01e22f29a6bffc72e0e4
luissoto7710 <luissoto7710@gmail.com>
2024-05-22 23:10:57 -0400

    My Commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class for geometric operations.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+        Raises an exception if the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
