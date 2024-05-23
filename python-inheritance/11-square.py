aebaa1d71cee2d453ea53f1dcd1a2924fc990c2a
luissoto7710 <luissoto7710@gmail.com>
2024-05-22 23:57:31 -0400

    My Commit

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module 11-square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """
+    Defines a Square, which is a kind of Rectangle.
+
+    Attributes:
+        __size (int): The side length of the square, private attribute.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square.
+
+        Args:
+            size (int): The side length of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to zero.
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Return the string representation of the square.
+
+        Returns:
+            str: The string description of the square.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
