b2e3dda8fb67d528712b1850ccf04692f83d6794
luissoto7710 <luissoto7710@gmail.com>
2024-05-23 12:55:40 -0400

    My Commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Module 1
-"""
+MyList = __import__('1-my_list').MyList
 
-
-class MyList(list):
-    """
-    inherits fro list.
-    """
-    def print_sorted(self):
-        """
-        Prints ascending.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
