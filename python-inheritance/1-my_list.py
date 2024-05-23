7f0f234eeec8b47378420135e5ff2a910f972d62
luissoto7710 <luissoto7710@gmail.com>
2024-05-22 23:59:15 -0400

    My Commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
-module defines the MyList class which inherits from list class and
-adds a method to print list in ascending order.
+module 1
 """
 
 
 class MyList(list):
     """
-    MyList class that inherits from list.
-
-    Methods:
-        print_sorted(): Prints the list in ascending order.
+    mylsit class
     """
-
     def print_sorted(self):
         """
-        Prints the elements of the list in ascending order.
+        print func.
         """
-        print(sorted(self))
+        sorted_list = sorted(self)
+        print(sorted_list)
