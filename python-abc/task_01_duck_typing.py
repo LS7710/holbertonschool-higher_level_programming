5910cb2ae309422ca0a1475784f11b6e4275d31d
luissoto7710 <luissoto7710@gmail.com>
2024-05-24 19:15:08 -0400

    My Commit

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -17,14 +17,13 @@ class Shape(ABC):
 
 
 class Circle(Shape):
-
     def __init__(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
         self.radius = radius
-
 
     def area(self):
         return pi * self.radius ** 2
-
 
     def perimeter(self):
         return 2 * pi * self.radius
