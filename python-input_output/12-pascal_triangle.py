1a998d8115e00deafd1afd97abfbf03617363dbb
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 22:51:30 -0400

    My Commit

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+mod 12
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle of height n.
+
+    Args:
+        n (int): The number of levels of the triangle to generate.
+
+    Returns:
+        List of lists of integers representing Pascal's triangle.
+    """
+    triangle = []
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
