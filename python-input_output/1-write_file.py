db69c4f4334a096371d63c42f8f59b9ce5c7ac9c
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 21:32:22 -0400

    My Commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+module 1.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes string to text file and returns number of characters.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The string to write into the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
