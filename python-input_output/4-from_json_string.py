6cdf626e5cb05d1a6cc91150ba09ff76b82845d2
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 21:45:26 -0400

    My Commit

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+module 4.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns object.
+
+    Args:
+        my_str (str): The JSON string to deserialize.
+
+    Returns:
+        object: data structure represented by string.
+    """
+
+    return json.loads(my_str)
