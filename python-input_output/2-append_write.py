6dc8d443b28c3c5f6ce23cb965ce2f91cb6a976b
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 21:20:21 -0400

    My Commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+module 1.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends string to end of a text file and returns the number of
+    characters added.
+
+    Args:
+        filename (str): name of file being appended.
+        text (str): string appended.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
