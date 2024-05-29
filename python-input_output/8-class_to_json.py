60bc593a1527407d8d0fccc26d3697ce52e8723a
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 22:30:29 -0400

    My Commit

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+module 8
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns
+
+    Args:
+        obj: An instance of a Class whose attributes are serializable.
+
+    Returns:
+        A dictionary containing all serializable attributes of obj.
+    """
+    return obj.__dict__
