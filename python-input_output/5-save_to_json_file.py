a1c2261963c0fad7fab605d72af9f94566c06641
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 21:51:42 -0400

    My Commit

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+module 5
+"""
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to file.
+
+    Args:
+        my_obj: The Python object to serialize.
+        filename (str): filename.
+
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
