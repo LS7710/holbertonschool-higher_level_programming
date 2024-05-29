b61158164ff6e21f68679011629f5dc5450a8209
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 22:40:50 -0400

    My Commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """
-module 7
+mod 7
+
+Uses:
+- 'save_to_json_file' to save the list to a file.
+- 'load_from_json_file' to load the existing list from the file.
+
+The script does not handle file permission exceptions or check for the validity of JSON content.
 """
+
 
 import sys
 import os
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
 def main():
-    """
-    this is here so it passes pep8, im tired.
-    """
     filename = "add_item.json"
 
     if os.path.exists(filename):
+        from load_from_json_file import load_from_json_file
         items = load_from_json_file(filename)
     else:
         items = []
 
     items.extend(sys.argv[1:])
 
+    from save_to_json_file import save_to_json_file
     save_to_json_file(items, filename)
 
 if __name__ == "__main__":
