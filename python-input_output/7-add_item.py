e3f3bf26efb65ceb89e25468d47eccb80768e59a
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 22:27:23 -0400

    My Commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,21 +4,19 @@ module 7
 """
 
 import sys
-import os.path
-
-
+import os
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 def main():
+    """
+    this is here so it passes pep8, im tired.
+    """
     filename = "add_item.json"
-    try:
-        if os.path.exists(filename):
-            items = load_from_json_file(filename)
-        else:
-            items = []
-    except FileNotFoundError:
 
+    if os.path.exists(filename):
+        items = load_from_json_file(filename)
+    else:
         items = []
 
     items.extend(sys.argv[1:])
