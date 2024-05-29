a7ebea08595748c5ec8119aa091425ac0f4856e2
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 21:57:02 -0400

    My Commit

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+module 6
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates obj.
+
+    Args:
+        filename (str): The name of the file
+
+    Returns:
+        object: obj.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
