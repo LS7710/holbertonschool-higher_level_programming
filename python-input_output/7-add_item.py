86a39c8f0bbd47de4f30d0a4563b4cf81bdaafc4
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 22:17:11 -0400

    My Commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+module 7
+"""
+
+import sys
+import os.path
+
+
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+def main():
+    filename = "add_item.json"
+    try:
+        if os.path.exists(filename):
+            items = load_from_json_file(filename)
+        else:
+            items = []
+    except FileNotFoundError:
+
+        items = []
+
+    items.extend(sys.argv[1:])
+
+    save_to_json_file(items, filename)
+
+if __name__ == "__main__":
+    main()
