b38006f0a843a0001b7c43dae7713c2d287b0bba
luissoto7710 <luissoto7710@gmail.com>
2024-05-28 23:07:47 -0400

    My Commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
 """
 mod 7
-
-Uses:
-- 'save_to_json_file' to save the list to a file.
-- 'load_from_json_file' to load the existing list from the file.
-
-The script does not handle file permission exceptions or check for the validity of JSON content.
 """
 
-def load_from_json_file(filename):
-    import json
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
-
-def save_to_json_file(my_obj, filename):
-    import json
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
 
 import sys
-import os
+from os.path import exists
 
-def main():
-    filename = "add_item.json"
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    if os.path.exists(filename):
-        from load_from_json_file import load_from_json_file
-        items = load_from_json_file(filename)
-    else:
-        items = []
+filename = "add_item.json"
 
-    items.extend(sys.argv[1:])
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
 
-    from save_to_json_file import save_to_json_file
-    save_to_json_file(items, filename)
+items.extend(sys.argv[1:])
 
-if __name__ == "__main__":
-    main()
+save_to_json_file(items, filename)
