fd027e635d94a999dc46516adb38772c29120373
luissoto7710 <luissoto7710@gmail.com>
2024-05-29 20:43:00 -0400

    added all files + task 1 + readme

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+mod 1
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize the provided dictionary and save it to the specified file.
+
+    Parameters:
+    data (dict): The dictionary to be serialized.
+    filename (str): The name of the file to save the JSON data.
+    """
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """
+    Load JSON data from the specified file and deserialize it into a dictionary.
+
+    Parameters:
+    filename (str): The name of the file to load the JSON data from.
+
+    Returns:
+    dict: The deserialized dictionary from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
