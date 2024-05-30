9d4be13b0c06b17ed9936921f9e011e0dc37bd5b
luissoto7710 <luissoto7710@gmail.com>
2024-05-29 20:53:46 -0400

    fix

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,50 +3,30 @@
 mod 0
 """
 
-import pickle
+import json
 
 
-class CustomObject:
-    def __init__(self, name, age, is_student):
-        self.name = name
-        self.age = age
-        self.is_student = is_student
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize the provided dictionary and save it to the specified file.
 
-    def display(self):
-        """
-        Print the object's attributes.
-        """
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
+    Parameters:
+    data (dict): The dictionary to be serialized.
+    filename (str): The name of the file to save the JSON data.
+    """
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-    def serialize(self, filename):
-        """
-        Serialize the current instance of CustomObject to a file.
-        
-        Parameters:
-        filename (str): The name of the file to save the serialized object.
-        """
-        try:
-            with open(filename, 'wb') as file:
-                pickle.dump(self, file)
-        except Exception as e:
-            print(f"An error occurred while serializing: {e}")
-    
-    @classmethod
-    def deserialize(cls, filename):
-        """
-        Deserialize an instance of CustomObject from a file.
-        
-        Parameters:
-        filename (str): The name of the file to load the serialized object from.
+def load_and_deserialize(filename):
+    """
+    Load JSON data from the specified file, deserialize to dictionary.
 
-        Returns:
-        CustomObject: The deserialized instance of CustomObject, or None if an error occurs.
-        """
-        try:
-            with open(filename, 'rb') as file:
-                return pickle.load(file)
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
-            print(f"An error occurred while deserializing: {e}")
-            return None
+    Parameters:
+    filename (str): The name of the file to load the JSON data from.
+    Returns:
+
+    dict: The deserialized dictionary from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
