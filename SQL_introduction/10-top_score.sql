4bff6323089578a59796a32eb02404dc1fb54617
luissoto7710 <luissoto7710@gmail.com>
2024-06-17 22:09:40 -0400

    My Commit

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,13 +1,3 @@
--- Create second_table if it does not exist
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
--- Insert specified records into second_table
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+-- List all records ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
 
