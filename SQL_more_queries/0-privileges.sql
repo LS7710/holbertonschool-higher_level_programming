7faacea7000ad0b30d00a41e01c88d36da5fa2d9
luissoto7710 <luissoto7710@gmail.com>
2024-06-18 23:28:12 -0400

    fix t1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,10 @@
--- List all privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Create the MySQL server user user_0d_1 with all privileges
 
--- List all privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Check if the user already exists and create if not
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
