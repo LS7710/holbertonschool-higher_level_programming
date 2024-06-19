7ff484f172f2748401e8bc5b4371b09a832b64a1
luissoto7710 <luissoto7710@gmail.com>
2024-06-18 23:29:05 -0400

    error fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,7 +3,7 @@
 -- Check if the user already exists and create if not
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user
+-- Check if the user already has all privileges and grant if not
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply changes
