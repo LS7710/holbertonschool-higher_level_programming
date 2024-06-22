51ba492e9ef92510171b573a33d17fabeb43a2bd
luissoto7710 <luissoto7710@gmail.com>
2024-06-21 23:54:28 -0400

    My Commit

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script prints the id of the State object with the name passed as argument
-from the database 'hbtn_0e_6_usa'.
+This script prints the id of the State object with the name passed as an
+argument from the database 'hbtn_0e_6_usa'.
 """
 
 import sys
@@ -10,25 +10,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    mysql_username, mysql_password, database_name, state_name_to_search = sys.argv[1:]
+    usr, pwd, db, name = sys.argv[1:]
 
-    # Create an engine that will connect to the MySQL server
     engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/{database_name}',
+        f'mysql+mysqldb://{usr}:{pwd}@localhost/{db}',
         pool_pre_ping=True
     )
 
-    # Bind the engine to the metadata of the Base class
     Base.metadata.create_all(engine)
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query for the state with the given name (SQL injection free)
-    state = session.query(State).filter(State.name == state_name_to_search).first()
+    state = session.query(State).filter(State.name == name).first()
 
-    # Print the state id if found, otherwise print 'Not found'
     if state:
         print(state.id)
     else:
