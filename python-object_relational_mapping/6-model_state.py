f21d6f1c14b729b44ac7f601f2cae722e695bf70
luissoto7710 <luissoto7710@gmail.com>
2024-06-21 23:04:21 -0400

    My Commit

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Defines the State class with SQLAlchemy
+"""
+
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Base instance for declarative system
+Base = declarative_base()
+
+class State(Base):
+    """State class:
+    - inherits from Base
+    - links to the MySQL table states
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine\
+        ('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa',\
+         pool_pre_ping=True)
+    Base.metadata.create_all(engine)
