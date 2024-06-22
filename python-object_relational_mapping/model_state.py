4995169f574aa7147881b22198f930b9f566ce0a
luissoto7710 <luissoto7710@gmail.com>
2024-06-21 23:28:01 -0400

    My Commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,14 +4,11 @@
 Defines the State class with SQLAlchemy
 """
 
-
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-
 # Base instance for declarative system
 Base = declarative_base()
-
 
 class State(Base):
     """State class:
@@ -22,9 +19,8 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-
 if __name__ == "__main__":
-    engine =\
-        create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa',
-    pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa',
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
