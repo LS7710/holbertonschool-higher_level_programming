812cb326abc61515bfb52318fd33e0b540db7963
luissoto7710 <luissoto7710@gmail.com>
2024-06-21 23:57:03 -0400

    My Commit

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,10 +10,10 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    usr, pwd, db = sys.argv[1:]
+    mysql_username, mysql_password, database_name = sys.argv[1:]
 
     engine = create_engine(
-        f'mysql+mysqldb://{usr}:{pwd}@localhost/{db}',
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/{database_name}',
         pool_pre_ping=True
     )
 
@@ -22,4 +22,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(City, State).join(State).order
+    # Join the states and cities tables to fetch the data sorted by city IDs
+    results = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
