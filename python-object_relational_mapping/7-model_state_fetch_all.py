05bfa0da5a5ea9a76fa9d7804f1dddf0cd9b6294
luissoto7710 <luissoto7710@gmail.com>
2024-06-21 23:40:48 -0400

    My Commit

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
 
     # Create an engine that will connect to the MySQL server
     engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/{database_name}',
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
+        f'@localhost/{database_name}', 
         pool_pre_ping=True
     )
 
