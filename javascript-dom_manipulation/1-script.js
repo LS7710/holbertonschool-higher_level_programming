ae886e7fdccdf9e83d6e70104d4d5200d08094f8
LS7710 <luissoto7710@gmail.com>
2024-07-26 16:25:54 -0400

    task 0-3 + fixes

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const redHeader = document.getElementById('red_header');
+
+    redHeader.addEventListener('click', function () {
+
+      document.querySelector('header').style.color = '#FF0000';
+    });
+  });
